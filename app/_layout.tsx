6bd7f9af31d61f1b74fafc45c276111c9036c4e6
rupali170 <nd751413@gmail.com>
2026-06-03 07:44:48 +0530

    Create _layout.tsx

---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+export default function RootLayout() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
+}
